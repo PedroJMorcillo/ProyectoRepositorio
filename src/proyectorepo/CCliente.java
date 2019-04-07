9732307ddaaf9725353852b0a64dcc465162edad
pedrojmorcillo <pedrojmorcillo@outlook.es>
2019-04-07 22:35:27 +0200

    Commit nuevo tan solo de la clase CCliente (que es lo nuevo que he añadido, para qué hacer un commit de todo el proyecto si solo añado esto)

---
+++ src/proyectorepo/CCliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectorepo;
+
+/**
+ *
+ * @author Pedro
+ */
+public class CCliente {
+    String nombre;
+    String apellido1;
+    
+    public CCliente(String n, String a){
+        nombre = n;
+        apellido1 = a;
+    }
+    
+    
+}
